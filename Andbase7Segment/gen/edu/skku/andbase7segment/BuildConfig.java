7510f90fccab8eff8f22d447295459726e6d5ff6
Honggyu <6398lhg@gmail.com>
2021-12-15 01:55:00 +0900

    base 7 segment, fpga 7 segment simple application

---
+++ Andbase7Segment/gen/edu/skku/andbase7segment/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.skku.andbase7segment;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Andfpga7Segment/gen/edu/skku/andfpga7segment/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.skku.andfpga7segment;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
